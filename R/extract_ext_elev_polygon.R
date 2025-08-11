df78e14ea52c52f01cc3faf8b01c60c88fc68f48
tadhg-moore <tadhg.moore6@gmail.com>
2025-08-12 11:21:11 +1200

    Add fun doc

--- R/extract_ext_elev_polygon.R
+++ R/extract_ext_elev_polygon.R
@@ -1,9 +1,22 @@
+#' Extract the largest polygon connected to the lake for calculating extended hypsograph
+#' @param dem_bath SpatRaster object with the bathymetry data.
+#' @param shoreline sf object with the shoreline of the lake.
+#' @param lake_elev numeric. The elevation of the lake.
+#' @param ext_elev numeric. The additional elevation to extend the lake surface.
+#' @return sf object with the largest polygon connected to the lake.
+#' @noRd
+#' @importFrom terra clamp ifel as.polygons
+#' @importFrom sf st_as_sf st_cast st_intersects
+#' @importFrom dplyr filter
 extract_ext_elev_polygon <- function(dem_bath, shoreline, lake_elev, ext_elev) {
-  # Extract largest polygon connected to the lake for calculating extended hypsograph
-  dem_cond <- terra::clamp(dem_bath, upper = (lake_elev + ext_elev), values = FALSE)
-  dem_cond <- terra::ifel(dem_bath > (lake_elev + ext_elev), NA, lake_elev + ext_elev)
+  # Extract largest polygon connected to the lake for calculating extended 
+  # hypsograph
+  dem_cond <- terra::clamp(dem_bath, upper = (lake_elev + ext_elev), 
+                           values = FALSE)
+  dem_cond <- terra::ifel(dem_bath > (lake_elev + ext_elev), NA, 
+                          lake_elev + ext_elev)
   # terra::plot(dem_cond)
-  plot(shoreline, add = TRUE)
+  # plot(shoreline, add = TRUE)
   ext_poly <- terra::as.polygons(dem_cond) |> 
     sf::st_as_sf() |> 
     sf::st_cast("POLYGON") 
