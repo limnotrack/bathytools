c40dd718abc6331ecba4ca1f794c816bc0d942a2
tadhg-moore <tadhg.moore6@gmail.com>
2025-08-25 09:09:32 +1200

    Remove redundant code

--- tests/testthat/test-rasterise_bathy.R
+++ tests/testthat/test-rasterise_bathy.R
@@ -41,28 +41,4 @@ test_that("generate contours from a bathymetry raster file", {
 
   shoreline2 <- get_shoreline(bathy_raster = bathy_raster)
   testthat::expect_s3_class(shoreline2, "sf")
-
-  # bathy_raster2 <- rasterise_bathy(shoreline = shoreline2, contours = contours,
-  #                                  crs = 2193, res = 8)
-  # tm_shape(bathy_raster) + tm_raster() + tm_shape(bathy_raster2) + tm_raster()
-  # par(mfrow = c(2, 1))
-  # bathy_raster3 <- terra::resample(bathy_raster2, bathy_raster)
-  # terra::plot(bathy_raster)
-  # terra::plot(bathy_raster2)
-  #
-  # diff <- bathy_raster - bathy_raster3
-  # tm_shape(diff) + tm_raster(style = "cont")
-  # diff |>
-  #   terra::values() |>
-  #   mean(na.rm = TRUE)
-  # diff |>
-  #   terra::values() |>
-  #   hist()
-  #   # median(na.rm = TRUE)
-  #
-  # hyps1 <- bathy_to_hypso(bathy_raster = bathy_raster)
-  # hyps2 <- bathy_to_hypso(bathy_raster = bathy_raster2)
-  # plot(hyps1$area, hyps1$depth, type = "l")
-  # lines(hyps2$area, hyps2$depth, col = "red")
-
 })
