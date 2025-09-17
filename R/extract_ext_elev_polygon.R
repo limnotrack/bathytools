6945a53e1fa206978a1bfe14e1763b100e3e7dc9
tadhg-moore <tadhg.moore6@gmail.com>
2025-09-18 11:56:59 +1200

    Update fun

--- R/extract_ext_elev_polygon.R
+++ R/extract_ext_elev_polygon.R
@@ -5,25 +5,39 @@
 #' @param ext_elev numeric. The additional elevation to extend the lake surface.
 #' @return sf object with the largest polygon connected to the lake.
 #' @noRd
-#' @importFrom terra clamp ifel as.polygons
+#' @importFrom terra clamp ifel as.polygons disagg subset relate vect project 
+#' crs
 #' @importFrom sf st_as_sf st_cast st_intersects
 #' @importFrom dplyr filter
 extract_ext_elev_polygon <- function(dem_bath, shoreline, lake_elev, ext_elev) {
   # Extract largest polygon connected to the lake for calculating extended 
   # hypsograph
-  dem_cond <- terra::clamp(dem_bath, upper = (lake_elev + ext_elev), 
-                           values = FALSE)
-  dem_cond <- terra::ifel(dem_bath > (lake_elev + ext_elev), NA, 
-                          lake_elev + ext_elev)
-  # terra::plot(dem_cond)
-  # plot(shoreline, add = TRUE)
-  ext_poly <- terra::as.polygons(dem_cond) |> 
-    sf::st_as_sf() |> 
-    sf::st_cast("POLYGON") 
+  # --- build a mask (1 = keep, NA = drop) ---
+  mask <- dem_bath <= (lake_elev + ext_elev)      # logical SpatRaster
+  mask <- terra::ifel(mask, 1, NA)                # 1 / NA
   
-  # Find which polygon contains the shoreline
-  poly <- ext_poly[sf::st_intersects(ext_poly, shoreline, sparse = FALSE), ]
-  # terra::plot(poly)
+  # --- convert to polygons, merging contiguous cells ---
+  ext_poly <- terra::as.polygons(mask, dissolve = TRUE) |> 
+    terra::disagg()
+  
+  shoreline_vect <- shoreline |> 
+    terra::vect() |> 
+    terra::project(terra::crs(dem_bath))
+  
+  # plot(shoreline, add = TRUE)
+  # ext_poly <- terra::as.polygons(dem_cond)
+  # terra::plot(ext_poly)
+  # terra::plot(shoreline_vect, add = TRUE)
+  # 
+  # Check intersection
+  rel <- terra::relate(ext_poly, shoreline_vect, relation = "intersects")
+
+  # Subset polygons
+  poly <- ext_poly |> 
+    terra::subset(rel[, 1])
+  
+  # terra::plot(poly, add = TRUE, border = "red", lwd = 2)
   
   return(poly)
 }
+
