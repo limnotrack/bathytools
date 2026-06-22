b8417af0d8f6ae82b9fcc1be7ab30fb577415801
Tadhg Moore <tadhg.moore6@gmail.com>
2026-06-23 11:20:53 +1200

    updatre args and add island detection

--- R/rasterise_bathy.R
+++ R/rasterise_bathy.R
@@ -2,9 +2,11 @@
 #'
 #' @inheritParams generate_depth_points
 #' @inheritParams interpolate_points
-#' @param method character interpolation method. Options are nearest neighbour 
-#' ('nn') (default), multilevel B-splines ('MBA'), thin plate spline ('tps'),
-#' and inverse distance weighting ('idw').
+#' @param method character interpolation method. Options are c('MBA', 'tps',
+#' 'nn', 'idw'). Default is 'nn' (nearest neighbor). 'MBA' uses the
+#' MBA::mba.surf function, 'tps' uses a thin plate spline from the fields
+#' package, 'nn' uses nearest neighbor interpolation from the terra package,
+#' and 'idw' uses inverse distance weighting from the terra package.
 #' @inheritParams sf::st_transform
 #' @inheritParams MBA::mba.surf
 #' 
@@ -24,9 +26,22 @@
 
 rasterise_bathy <- function(shoreline, islands = NULL, depth_points = NULL,
                             contours = NULL, res = 2, subsample = TRUE,
-                            crs, method = "nn", print_plot = TRUE,
+                            crs, method = c("MBA", "tps", "nn", "idw"),
+                            print_plot = TRUE,
                             n = 1, m = 1, h = 8) {
 
+  method <- rlang::arg_match(method)
+  
+  missing_islands <- detect_islands(shoreline)
+  if (!is.null(missing_islands)) {
+    if (is.null(islands)) {
+      islands <- missing_islands
+    } else {
+      islands <- dplyr::bind_rows(islands, missing_islands)
+    }
+  }
+  shoreline <- detect_shoreline(shoreline)
+  
   # cli::cli_progress_step("Generating depth points for interpolation")
   depth_points <- generate_depth_points(shoreline = shoreline, 
                                         islands = islands,
@@ -36,7 +51,8 @@ rasterise_bathy <- function(shoreline, islands = NULL, depth_points = NULL,
 
   # cli::cli_progress_step("Interpolating depth points to raster")
   bathy <- interpolate_points(depth_points = depth_points, 
-                              shoreline = shoreline, islands = islands,
+                              shoreline = shoreline, 
+                              islands = islands,
                               res = res, method = method, n = n, m = m, h = h,
                               crs = crs, print_plot = print_plot)
   if (!is.null(islands)) {
